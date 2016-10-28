939d56df8c4f8b907ee14c77f94df785821dde67
Axel Simón <asimon@gisce.net>
2016-10-28 16:28:02 +0200

    Test for AccountService has been added

---
+++ caes_practica_1/src/test/java/org/udg/caes/banking/service/TestAccountService.java
@@ -0,0 +1,38 @@
+package org.udg.caes.banking.service;
+
+import mockit.Expectations;
+import mockit.Injectable;
+import mockit.Mocked;
+import mockit.Tested;
+import org.junit.Test;
+import org.udg.caes.banking.entity.Account;
+import org.udg.caes.banking.exceptions.AccountNotFound;
+import org.udg.caes.banking.exceptions.EntityNotFound;
+import org.udg.caes.banking.manager.EntityManager;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by deidas on 28/10/16.
+ */
+public class TestAccountService {
+    @Tested
+    AccountService acs;
+
+    @Test
+    public void GetAccountOK(@Injectable final EntityManager em, @Mocked final Account ac) throws Exception{
+        new Expectations(){{
+            em.get("foo", Account.class); result = ac;
+        }};
+        Account res = acs.getAccount("foo");
+        assertSame(res, ac);
+    }
+
+    @Test(expected = AccountNotFound.class)
+    public void GetAccountFail(@Injectable final EntityManager em) throws Exception{
+        new Expectations(){{
+            em.get("foo", Account.class); result = new EntityNotFound();
+        }};
+        acs.getAccount("foo");
+    }
+}
---
+++ caes_practica_1/src/test/java/org/udg/caes/banking/service/TestClientService.java
@@ -0,0 +1,7 @@
+package org.udg.caes.banking.service;
+
+/**
+ * Created by deidas on 28/10/16.
+ */
+public class TestClientService {
+}
