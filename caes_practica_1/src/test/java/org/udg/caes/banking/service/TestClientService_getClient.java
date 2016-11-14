2bfaa06801fff98851b81462f8189b76b944ad79
Axel Simón <asimon@gisce.net>
2016-11-14 20:33:39 +0100

    GetClientOK test has been added

--- caes_practica_1/src/test/java/org/udg/caes/banking/service/TestClientService_getClient.java
+++ caes_practica_1/src/test/java/org/udg/caes/banking/service/TestClientService_getClient.java
@@ -1,7 +1,14 @@
 package org.udg.caes.banking.service;
 
+import mockit.Expectations;
+import mockit.Injectable;
+import mockit.Mocked;
 import mockit.Tested;
 import org.junit.Test;
+import org.udg.caes.banking.entity.Client;
+import org.udg.caes.banking.manager.EntityManager;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by deidas on 28/10/16.
@@ -11,7 +18,11 @@ public class TestClientService_getClient {
     ClientService cs;
 
     @Test
-    public void GetClientOK() throws Exception{
-        
+    public void GetClientOK(@Injectable final EntityManager em, @Mocked final Client cli) throws Exception{
+        new Expectations(){{
+            em.get("foo", Client.class); result = cli;
+        }};
+        Client res = cs.getClient("foo");
+        assertSame(cli, res);
     }
 }
