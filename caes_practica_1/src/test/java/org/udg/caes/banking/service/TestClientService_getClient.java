aa32562a1728a1c835099e8b9ac90b32d2310371
Axel Simón <asimon@gisce.net>
2016-11-14 20:04:14 +0100

    Initial commit for TestClientService_getClient

--- caes_practica_1/src/test/java/org/udg/caes/banking/service/TestClientService_getClient.java
+++ caes_practica_1/src/test/java/org/udg/caes/banking/service/TestClientService_getClient.java
@@ -1,7 +1,17 @@
 package org.udg.caes.banking.service;
 
+import mockit.Tested;
+import org.junit.Test;
+
 /**
  * Created by deidas on 28/10/16.
  */
 public class TestClientService_getClient {
+    @Tested
+    ClientService cs;
+
+    @Test
+    public void GetClientOK() throws Exception{
+        
+    }
 }
