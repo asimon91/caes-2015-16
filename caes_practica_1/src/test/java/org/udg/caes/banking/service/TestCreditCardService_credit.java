31f252fdd23f1b9838ab53e03cfe9b51442eb490
Axel Simón <asimon@gisce.net>
2016-11-17 19:26:08 +0100

    CreditCardService credit method tests has been added

--- caes_practica_1/src/test/java/org/udg/caes/banking/service/TestCreditCardService_credit.java
+++ caes_practica_1/src/test/java/org/udg/caes/banking/service/TestCreditCardService_credit.java
@@ -4,6 +4,8 @@ import mockit.*;
 import org.junit.Test;
 import org.udg.caes.banking.entity.Account;
 import org.udg.caes.banking.entity.CreditCard;
+import org.udg.caes.banking.exceptions.CreditExceeded;
+import org.udg.caes.banking.exceptions.PersistenceException;
 import org.udg.caes.banking.manager.EntityManager;
 
 import static org.junit.Assert.*;
@@ -16,7 +18,7 @@ public class TestCreditCardService_credit {
     CreditCardService ccs;
 
     @Test
-    public void CreditOK(@Injectable final EntityManager em, @Mocked final CreditCard visa) throws Exception{
+    public void CreditOK(@Injectable final EntityManager em, @Mocked final CreditCard visa) throws Exception {
         new Expectations(){{
             visa.getCredit(); result = 1500;
             visa.getMaxCredit(); result = 2000;
@@ -24,6 +26,26 @@ public class TestCreditCardService_credit {
         ccs.credit(visa, 500);
         new Verifications(){{
             em.persist(visa); times = 1;
+            visa.credit(500); times = 1;
         }};
+    }
+
+    @Test(expected = CreditExceeded.class)
+    public void CreditExceededError(@Mocked final CreditCard visa) throws Exception {
+        new Expectations(){{
+            visa.getCredit(); result = 1500;
+            visa.getMaxCredit(); result = 3000;
+        }};
+        ccs.credit(visa, 2000);
+    }
+
+    @Test(expected = PersistenceException.class)
+    public void CreditPersistenceExceptionError(@Injectable final EntityManager em, @Mocked final CreditCard visa) throws Exception {
+        new Expectations(){{
+            visa.getCredit(); result = 100;
+            visa.getMaxCredit(); result = 2000;
+            em.persist(visa); result = new PersistenceException();
+        }};
+        ccs.credit(visa, 400);
     }
 }
