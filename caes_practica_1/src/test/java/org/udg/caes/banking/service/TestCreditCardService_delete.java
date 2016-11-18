9a8bcab0c9e1455cf1ded9bb5800c5ab6c1a6cba
Axel Simón <asimon@gisce.net>
2016-11-18 15:50:18 +0100

    Tests for CreditCardService delete method

---
+++ caes_practica_1/src/test/java/org/udg/caes/banking/service/TestCreditCardService_delete.java
@@ -0,0 +1,75 @@
+package org.udg.caes.banking.service;
+
+import mockit.*;
+import org.junit.Test;
+import org.udg.caes.banking.entity.Account;
+import org.udg.caes.banking.entity.CreditCard;
+import org.udg.caes.banking.exceptions.NotEnoughBalance;
+import org.udg.caes.banking.exceptions.PersistenceException;
+import org.udg.caes.banking.manager.EntityManager;
+
+/**
+ * Created by deidas on 18/11/16.
+ */
+public class TestCreditCardService_delete {
+    @Tested
+    CreditCardService ccs;
+
+    @Injectable
+    EntityManager em;
+
+    @Mocked
+    Account acc;
+
+    @Mocked
+    CreditCard visa;
+
+    @Test
+    public void DeleteOK() throws Exception {
+        new Expectations(){{
+            em.getAccountAssociated(visa); //result = acc, not necessary
+        }};
+        new Expectations(CreditCardService.class){{
+            ccs.charge(visa, acc);
+        }};
+        ccs.delete(visa);
+        new VerificationsInOrder(){{
+            ccs.charge(visa, acc); times = 1;
+            em.delete(visa); times = 1;
+        }};
+    }
+
+    @Test(expected = NotEnoughBalance.class)
+    public void DeleteNotEnoughBalanceError() throws Exception {
+        new Expectations(){{
+            em.getAccountAssociated(visa); //result = acc, not necessary
+        }};
+        new Expectations(CreditCardService.class){{
+           ccs.charge(visa, acc); result = new NotEnoughBalance();
+        }};
+        ccs.delete(visa);
+    }
+
+    @Test(expected = PersistenceException.class)
+    public void DeleteChargePersistenceExceptionError() throws Exception {
+        new Expectations(){{
+            em.getAccountAssociated(visa);
+        }};
+        new Expectations(CreditCardService.class){{
+            ccs.charge(visa, acc); result = new PersistenceException();
+        }};
+        ccs.delete(visa);
+    }
+
+    @Test(expected = PersistenceException.class)
+    public void DeletePersistenceExceptionEntityManagerError() throws Exception {
+        new Expectations(){{
+            em.getAccountAssociated(visa);
+            em.delete(visa); result = new PersistenceException();
+        }};
+        new Expectations(CreditCardService.class){{
+            ccs.charge(visa, acc);
+        }};
+        ccs.delete(visa);
+    }
+}
