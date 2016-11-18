897793f3692cb8458cd1a6ae7c519043c0425e0a
Axel Simón <asimon@gisce.net>
2016-11-18 15:50:42 +0100

    New tests has been added to CreditCardService charge method

--- caes_practica_1/src/test/java/org/udg/caes/banking/service/TestCreditCardService_charge.java
+++ caes_practica_1/src/test/java/org/udg/caes/banking/service/TestCreditCardService_charge.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.udg.caes.banking.entity.Account;
 import org.udg.caes.banking.entity.CreditCard;
 import org.udg.caes.banking.exceptions.NotEnoughBalance;
+import org.udg.caes.banking.exceptions.PersistenceException;
 import org.udg.caes.banking.manager.EntityManager;
 
 import static org.junit.Assert.assertEquals;
@@ -16,16 +17,15 @@ public class TestCreditCardService_charge {
     @Tested
     CreditCardService ccs;
 
-    Account acc = new Account("acc", 1000);
-
     @Test
-    public void ChargeOK(@Injectable final EntityManager em, @Mocked final CreditCard visa) throws Exception {
+    public void ChargeOK(@Injectable final EntityManager em, @Mocked final CreditCard visa, @Mocked final Account acc) throws Exception {
         new Expectations(){{
             visa.getCredit(); result = 400;
+            acc.getBalance(); result = 1000;
         }};
         ccs.charge(visa, acc);
-        assertEquals(acc.getBalance(), 600);
-        new VerificationsInOrder(){{
+        new Verifications(){{
+            acc.debit(400); times = 1;
             visa.reset(); times = 1;
             em.persist(visa); times = 1;
             em.persist(acc); times = 1;
@@ -33,18 +33,30 @@ public class TestCreditCardService_charge {
     }
 
     @Test(expected = NotEnoughBalance.class)
-    public void ChargeNotEnoughBalance(@Mocked final CreditCard visa) throws Exception {
+    public void ChargeNotEnoughBalance(@Mocked final CreditCard visa, @Mocked final Account acc) throws Exception {
         new Expectations(){{
             visa.getCredit(); result = 1500;
+            acc.getBalance(); result = 1000;
         }};
         ccs.charge(visa, acc);
     }
 
-//    @Test(expected = NotEnoughBalance.class)
-//    public void ChargeNotEnoughBalanceSecond(@Mocked final CreditCard visa) throws Exception {
-//        new Expectations(){{
-//            visa.getCredit(); returns(500, 1500);
-//        }};
-//        ccs.charge(visa, acc);
-//    }
+    @Test(expected = PersistenceException.class)
+    public void ChargeCreditCardPersistenceExceptionError(@Injectable final EntityManager em, @Mocked final CreditCard visa, @Mocked final Account acc) throws Exception {
+        new Expectations(){{
+            visa.getCredit(); result = 800;
+            acc.getBalance(); result = 1000;
+            em.persist(visa); result = new PersistenceException();
+        }};
+        ccs.charge(visa, acc);
+    }
+    @Test(expected = PersistenceException.class)
+    public void ChargeAccountPersistenceExceptionError(@Injectable final EntityManager em, @Mocked final CreditCard visa, @Mocked final Account acc) throws Exception {
+        new Expectations(){{
+            visa.getCredit(); result = 200;
+            acc.getBalance(); result = 1000;
+            em.persist(acc); result = new PersistenceException();
+        }};
+        ccs.charge(visa, acc);
+    }
 }
